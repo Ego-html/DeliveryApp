5de9aaa7c02232580d15bd9b1d7ae18b330a3cdd
Ego-html <62375400+Ego-html@users.noreply.github.com>
2023-06-01 13:42:10 +0300

    DeliveryApp add data to localStorage

--- script/shoppingpage.js
+++ script/shoppingpage.js
@@ -6,11 +6,35 @@ function addToCart(event) {
   if (target.tagName != "BUTTON") return;
   let text = target.previousSibling;
   let currentPrice = text.previousSibling;
-  localStorage.setItem(
-    "burger1",
-    JSON.stringify({ burger1: currentPrice.innerHTML })
-  );
-  let obj = JSON.parse(localStorage.burger1);
-  console.log(obj.burger1);
+  let productItemAction = currentPrice.parentElement;
+  let productItemActionText = productItemAction.previousSibling;
+  let productItemText = productItemActionText.previousSibling;
+  let productItemTitleText = productItemText.previousSibling;
+  let productItemTitle = productItemTitleText.previousSibling;
+  let productItemPictureText = productItemTitle.previousSibling;
+  let productItemPicture = productItemPictureText.previousSibling;
+  let img = productItemPicture.children[0].src;
+  if (localStorage.length !== 0) {
+    let parse = JSON.parse(localStorage.burgers);
+    parse.push(
+      { price: currentPrice.innerHTML },
+      { discription: productItemText.innerHTML },
+      { name: productItemTitle.innerHTML },
+      { img: img }
+    );
+    localStorage.clear();
+    localStorage.setItem("burgers", JSON.stringify(parse));
+  } else {
+    localStorage.setItem(
+      "burgers",
+      JSON.stringify([
+        { price: currentPrice.innerHTML },
+        { discription: productItemText.innerHTML },
+        { name: productItemTitle.innerHTML },
+        { img: img },
+      ])
+    );
+  }
 }
+
 document.addEventListener("click", addToCart);
